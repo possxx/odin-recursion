5b60dd7d8e7f3f825a1ba4f93874c44be8d42730
possxx <168581964+possxx@users.noreply.github.com>
2024-10-10 17:20:20 +0200

    Add recursive function for merge sort algorithm

---
+++ mergesort.js
@@ -0,0 +1,23 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const sortedArr = [];
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      sortedArr.push(left.shift());
+    } else {
+      sortedArr.push(right.shift());
+    }
+  }
+  return [...sortedArr, ...left, ...right];
+}
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
